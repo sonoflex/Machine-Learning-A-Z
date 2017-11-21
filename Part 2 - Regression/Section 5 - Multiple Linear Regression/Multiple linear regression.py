199f51563d59543a65c809118a40626b4d258248
Sebastian Pomp <sebastian@12bft.de>
2017-11-21 17:04:29 +0100

    up

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple linear regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple linear regression.py
@@ -25,15 +25,10 @@ X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
+# Avoiding the dummy variable trap
+X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
