d2c4ba1ef13f447952ffea75c29f1e621e490056
Sebastian Pomp <sebastian@12bft.de>
2017-11-22 14:26:18 +0100

    multiple linear regression

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple linear regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple linear regression.py
@@ -32,3 +32,36 @@ X = X[:, 1:]
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
+# Fitting multiple linear regression to the trainingset
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the testset results
+y_pred = regressor.predict(X_test)
+
+# Building the optimal model using backward elimination
+import statsmodels.formula.api as sm
+# Adding a collum of once for the constant b0
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1) 
+# Step 2:initialize the optimal set of variables and include all collums
+# and fit the full modell with all possible predictors
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# Step 3: Consider the predictor with the highast p-value (Possibility value, the lower, the more significant is the realated independent variable)
+regressor_OLS.summary()
+# Step 5: 1. itteration
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# Step 3: Consider the predictor with the highast p-value (Possibility value, the lower, the more significant is the realated independent variable)
+regressor_OLS.summary()
+# Step 5: 2. itteration
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# Step 3: Consider the predictor with the highast p-value (Possibility value, the lower, the more significant is the realated independent variable)
+regressor_OLS.summary()
+# Step 5: 3. itteration
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# Step 3: Consider the predictor with the highast p-value (Possibility value, the lower, the more significant is the realated independent variable)
+regressor_OLS.summary()
