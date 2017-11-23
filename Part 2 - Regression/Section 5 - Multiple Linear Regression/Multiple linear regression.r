d8ab18c9e004bf68595638e91e584d90d2005455
Sebastian Pomp <sebastian@12bft.de>
2017-11-23 16:58:32 +0100

    multiple linear regression in R

---
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple linear regression.r
@@ -0,0 +1,28 @@
+# Multiple linear regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting multiple linear regression to the training set
+# the "." in the formular is short for "R.D.Spend + Adminitration + Marketing.Spend + State"
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+summary(regressor)
+
+# Predicting the testset results
+y_pred = predict(regressor, newdata = test_set)
+
+print(y_pred)
